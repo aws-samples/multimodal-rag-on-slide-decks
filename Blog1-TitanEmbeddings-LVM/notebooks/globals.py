9878961ecd3d6515146eb3b205c25e3b5de51f71
EC2 Default User <ec2-user@ip-172-16-10-53.ec2.internal>
2024-01-07 00:09:18 +0000

    code cleanup

--- Blog1-TitanEmbeddings-LVM/notebooks/globals.py
+++ Blog1-TitanEmbeddings-LVM/notebooks/globals.py
@@ -1,31 +1,47 @@
+"""
+Global variables used throughout the code.
+"""
 import os
 import boto3
-from sagemaker.session import Session
+import sagemaker
 
-# global constants
-HF_MODEL_ID = "anymodality/llava-v1.5-7b"
-HF_MODEL_NAME = "model_llava-v1.5-7b.tar.gz"
-HF_TASK = "question-answering"
-TRANSFORMERS_VERSION = "4.28.1"
-PYTORCH_VERSION = "2.0.0"
-PYTHON_VERSION = "py310"
+# model deployment
+HF_MODEL_ID = "anymodality/llava-v1.5-13b"
+HF_MODEL_ID: str = "anymodality/llava-v1.5-7b"
 
-BUCKET_NAME = Session().default_bucket()
-BUCKET_PREFIX = "multimodal"
-BUCKET_EMB_PREFIX = "multimodal/osi-embeddings-json"
-BUCKET_IMG_PREFIX = "multimodal/img"
+HF_TASK: str = "question-answering"
+TRANSFORMERS_VERSION: str = "4.28.1"
+PYTORCH_VERSION: str = "2.0.0"
+PYTHON_VERSION: str = "py310"
 
-AWS_REGION = boto3.Session().region_name
-OS_SERVICE = "aoss"
-OS_INDEX = "multimodalslidesindex"
+# S3 bucket strucutre, we use the default sagemaker bucket in the current region
+BUCKET_NAME: str = sagemaker.Session().default_bucket()
+BUCKET_PREFIX: str = "multimodal"
+BUCKET_EMB_PREFIX: str = f"{BUCKET_PREFIX}/osi-embeddings-json"
+BUCKET_IMG_PREFIX: str = f"{BUCKET_PREFIX}/img"
 
-IMAGE_DIR = "img"
-B64_ENCODED_IMAGES_DIR = os.path.join(IMAGE_DIR, "b64_images")
+# Amazon Titan multimodal model
+AWS_REGION: str = boto3.Session().region_name
+FMC_URL: str = f"https://bedrock-runtime.{AWS_REGION}.amazonaws.com"
+FMC_MODEL_ID: str = "amazon.titan-embed-image-v1"
+ACCEPT_ENCODING: str = "application/json"
+CONTENT_ENCODING: str = "application/json"
 
-FMC_URL = "https://bedrock-runtime.us-east-1.amazonaws.com"
-FMC_MODEL_ID = "amazon.titan-embed-image-v1"
-ACCEPT_ENCODING = "application/json"
-CONTENT_ENCODING = "application/json"
+# model.tar.gz path in S3
+S3_MODEL_URI: str = os.path.join("s3://", BUCKET_NAME, BUCKET_PREFIX, os.path.basename(HF_MODEL_ID))
 
-S3_MODEL_URI=os.path.join("s3://", BUCKET_NAME, BUCKET_PREFIX, os.path.basename(HF_MODEL_ID))
-SLIDE_DECK = "https://d1.awsstatic.com/events/Summits/torsummit2023/CMP301_TrainDeploy_E1_20230607_SPEdited.pdf"
+# Amazon OpenSearch Service Serverless
+OS_SERVICE: str = "aoss"
+
+# local files and folder structure
+IMAGE_DIR: str = "img"
+IMAGE_FILE_EXTN: str = ".jpg"
+B64_ENCODED_IMAGES_DIR: str = os.path.join(IMAGE_DIR, "b64_images")
+ENDPOINT_FILENAME: str = "endpoint.txt"
+
+# this is the slide deck to which we will be talking to. Replace with your slide deck's URL to analyze a different deck
+SLIDE_DECK: str = "https://d1.awsstatic.com/events/Summits/torsummit2023/CMP301_TrainDeploy_E1_20230607_SPEdited.pdf"
+
+# AWS CloudFormation stack that created the resources for this blog post including this notebook
+# if a different name is used while creating the CloudFormation stack then change this to match the name you used
+CFN_STACK_NAME: str = "multimodal-stack"
--- Blog1-TitanEmbeddings-LVM/notebooks/utils.py
+++ Blog1-TitanEmbeddings-LVM/notebooks/utils.py
@@ -1,27 +1,46 @@
+"""
+Utility functions for S3 and CloudFormation uised by the rest of the code
+"""
 import os
 import boto3
+import logging
+import globals as g
+from typing import List
 from sagemaker.s3 import S3Uploader
-import globals
+
+logger = logging.getLogger(__name__)
 
 s3 = boto3.client('s3')
 
-def create_s3_structure():
-    s3.put_object(Bucket=BUCKET_NAME, Key=(BUCKET_PREFIX+'/'))
-    s3.put_object(Bucket=BUCKET_NAME, Key=(BUCKET_EMB_PREFIX+'/'))
-    s3.put_object(Bucket=BUCKET_NAME, Key=(BUCKET_IMG_PREFIX+'/'))
-
-def upload_to_s3(local_file_path:str, bucket_prefix:str):
+def upload_to_s3(local_file_path:str, bucket_name: str, bucket_prefix:str) -> None:
+    global s3
     try:
         with open(local_file_path, 'rb') as file:
             s3_key = os.path.join(bucket_prefix, os.path.basename(local_file_path))
-            s3.upload_fileobj(file, BUCKET_NAME, s3_key)
-            logger.info(f"File {local_file_path} uploaded to {BUCKET_NAME}/{s3_key}.")
+            s3.upload_fileobj(file, bucket_name, s3_key)
+            logger.info(f"File {local_file_path} uploaded to {bucket_name}/{s3_key}.")
     except Exception as e:
-        logger.error(f"Error uploading file to S3: {e}")
-        
-def download_from_s3():
-    images = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=BUCKET_IMG_PREFIX)['Contents']
+        logger.error(f"error uploading file to S3: {e}")
+
+
+def download_image_files_from_s3(bucket:str, bucket_image_prefix:str, local_image_dir:str, image_file_extn:str) -> List:
+    images = s3.list_objects_v2(Bucket=bucket, Prefix=bucket_image_prefix)['Contents']
+    local_file_paths: List = []
     for img in images:
-        if img['Key'].endswith('.jpg'):
-            file_path = os.path.join(IMAGE_DIR, os.path.basename(img['Key']))
-            s3.download_file(BUCKET_NAME, img['Key'], file_path)
+        if img['Key'].endswith(image_file_extn):
+            file_path = os.path.join(local_image_dir, os.path.basename(img['Key']))            
+            s3.download_file(bucket, img['Key'], file_path)
+            logger.info(f"downloaded {bucket}/{img['Key']} to {file_path}")
+            local_file_paths.append(file_path)
+    return local_file_paths 
+            
+            
+def get_cfn_outputs(stackname: str) -> List:  
+    cfn = boto3.client('cloudformation')
+    outputs = {}
+    stacks = cfn.describe_stacks(StackName=stackname)['Stacks']
+    if stacks == []:
+        return None
+    for output in stacks[0]['Outputs']:
+        outputs[output['OutputKey']] = output['OutputValue']
+    return outputs
